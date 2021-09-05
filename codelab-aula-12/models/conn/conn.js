c6d5e246babb72eb024e69cc8879acad187f28f3
ribeiroedu <ed.almeidaribeiro@gmail.com>
2021-09-05 13:48:54 -0300

    commit 05.09

--- codelab-aula-12/models/conn/conn.js
+++ codelab-aula-12/models/conn/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function Conn(url, porta, banco){
-    mongoose.connect(`mongodb://${url}:${porta}/${banco}`, {
+    mongoose.connect(`mongodb+srv://dbAdmin:2xIkI0mj360mByWa@blue-db.mtq9z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(() => {
