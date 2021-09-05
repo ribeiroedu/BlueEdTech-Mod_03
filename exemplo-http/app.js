ffd4b83b3f672435999556858af3077e279175f1
ribeiroedu <ed.almeidaribeiro@gmail.com>
2021-09-05 13:54:28 -0300

    commit 05.09

--- exemplo-http/app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-// console.log(http);
-
-http.createServer(function(req, res) {
-    res.end('<h1>Ola</h1>');
-}).listen(3000); //localhost:3000 é uma rota
-
-console.log('Meu servidor está rodando');
